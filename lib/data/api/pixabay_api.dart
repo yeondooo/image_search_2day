0395a065194029c173f9fc81201fcea753e979b9
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-03 22:13:01 +0900

    fix: getImages 파라미터 타입 추가

--- lib/data/api/pixabay_api.dart
+++ lib/data/api/pixabay_api.dart
@@ -3,7 +3,7 @@ import 'package:image_search_2day/data/dto/pixabay_result_dto.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi {
-  Future<PixabayResultDto> getImages(query) async {
+  Future<PixabayResultDto> getImages(String query) async {
     final url =
         'https://pixabay.com/api/?key=37971457-4bad1cc05aa70353cb5a9d52e&q=$query&image_type=photo';
     final http.Response response = await http.get(Uri.parse(url));
