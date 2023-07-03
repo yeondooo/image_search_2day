b27253e0f5bfce4b98f722c65fb507bcfe23c84f
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-03 22:13:44 +0900

    feat: main screen 구현

---
+++ lib/ui/main/main_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_2day/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('이미지 검색 앱'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: '이미지를 검색해 보세요',
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  viewModel.fetchImages(controller.text);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: viewModel.photos.length,
+              itemBuilder: (context, index) {
+                final photo = viewModel.photos[index];
+                return Hero(
+                  tag: photo.id,
+                  child: GestureDetector(
+                    onTap: () {
+                      context.push('/detail', extra: photo);
+                    },
+                    child: Image.network(
+                      photo.url,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
